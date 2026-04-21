04bfd87407b58b1f86637dd865c1f9453eed8fb1
assm <chabaneanssum@gmail.com>
2026-04-21 14:37:49 +0400

    tout modification a et fait

--- fany_functions.sh
+++ fany_functions.sh
@@ -1,17 +1,15 @@
-$ gco="ajout la fonction d indtalation"
-alias gco="git commit"
-# resultat : git commit-m '[dev]ajout devla fonctiom d instalation]'
-
 gco() {
 	git rev-parse --is-insaid-work-tree > /dev/nul 2>&1
 	if [ $? -ne 0 ];then
-		echo"Erreur :pas une depotngit"
+		echo"Erreur :pas une depot git"
 		return 1
 	fi
 	
 	branr=$(git branch --show-courrent)
 	if [ -z "$1" ]; then
 		echo "usege : gco \"message\""
+	return 1
 	fi
 
  git commit - m "[$branch] $1"
+}
